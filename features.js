47e3ba00a5aaa919279d28832c4ece10817e60f0
Govind-Kumar1 <136782602+Govind-Kumar1@users.noreply.github.com>
2023-09-17 12:18:32 +0530

    initial-commit

---
+++ features.js
@@ -0,0 +1,8 @@
+// const gfName = "MrsRandom";
+
+// // module.exports = gfName;
+// export default gfName;
+
+export const  generateLovePercent = () =>{
+    return '${~~(Math.random()*100)}%';
+};
